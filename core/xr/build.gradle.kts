b0a3b066d4f30612924e926ec4cfc0d2f46daa7a
Dereck Bridie <dbridie@google.com>
2025-08-28 18:48:56 +0200

    Add XR libraries and a new core/xr project with XR utils

---
+++ core/xr/build.gradle.kts
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+android {
+    namespace = "com.android.developers.androidify.xr"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+
+}
+
+dependencies {
+    implementation(libs.androidx.xr.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(projects.core.util)
+    implementation(projects.core.theme)
+}
---
+++ core/xr/src/main/java/com/android/developers/androidify/xr/SpatialUiModes.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.developers.androidify.xr
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.xr.compose.platform.LocalSession
+import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.platform.LocalSpatialConfiguration
+import androidx.xr.compose.platform.SpatialCapabilities
+import androidx.xr.scenecore.scene
+
+/** Check if the device is XR-enabled, but is not yet rendering spatial UI. */
+@Composable
+fun couldRequestFullSpace(): Boolean {
+    return LocalSpatialConfiguration.current.hasXrSpatialFeature && !LocalSpatialCapabilities.current.isSpatialUiEnabled
+}
+
+/** Check if the device is XR-enabled and is rendering spatial UI. */
+@Composable
+fun SpatialCapabilities.couldRequestHomeSpace(): Boolean {
+    if (!LocalSpatialConfiguration.current.hasXrSpatialFeature) return false
+    return isSpatialUiEnabled
+}
+
+/** Default styling for an IconButton with a home space button and behavior. */
+@Composable
+fun RequestHomeSpaceIconButton(modifier: Modifier = Modifier) {
+    val session = LocalSession.current ?: return
+
+    OutlinedIconButton(
+        modifier = modifier,
+        colors = IconButtonDefaults.outlinedIconButtonColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+        ),
+        onClick = {
+            session.scene.requestHomeSpaceMode()
+        },
+    ) {
+        Icon(
+            modifier = Modifier
+                .fillMaxSize(),
+            imageVector = ImageVector.vectorResource(R.drawable.collapse_content_24px),
+            contentDescription = "To Home Space Mode",
+        )
+    }
+}
+
+@Composable
+fun FullSpaceIcon(modifier: Modifier = Modifier) {
+    Icon(
+        modifier = modifier,
+        imageVector = ImageVector.vectorResource(R.drawable.expand_content_24px),
+        contentDescription = "To Full Space Mode",
+    )
+}
