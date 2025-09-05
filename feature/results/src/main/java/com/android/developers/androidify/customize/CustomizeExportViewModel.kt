8e060fe23e2e5df29a2d0c1650cf1f8f0cdeab65
Rebecca Franks <riggaroo@google.com>
2025-09-05 17:46:06 +0100

    Revert "Refactor image saving and loading in Creation and Customize features"
    This reverts commit 7bb6ccd7a4daec243177760354de6aadbb337d92.

--- feature/results/src/main/java/com/android/developers/androidify/customize/CustomizeExportViewModel.kt
+++ feature/results/src/main/java/com/android/developers/androidify/customize/CustomizeExportViewModel.kt
@@ -17,14 +17,12 @@ package com.android.developers.androidify.customize
 
 import android.app.Application
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.application
 import androidx.lifecycle.viewModelScope
 import com.android.developers.androidify.RemoteConfigDataSource
 import com.android.developers.androidify.data.ImageGenerationRepository
@@ -35,7 +33,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -243,7 +240,7 @@ class CustomizeExportViewModel @Inject constructor(
                 return@launch
             }
 
-            val image = state.value.exportImageCanvas.imageUri?.let { uri -> convertUriToBitmap(uri) }
+            val image = state.value.exportImageCanvas.imageBitmap
             if (image == null) {
                 return@launch
             }
@@ -297,24 +294,6 @@ class CustomizeExportViewModel @Inject constructor(
     fun changeSelectedTool(tool: CustomizeTool) {
         _state.update {
             it.copy(selectedTool = tool)
-        }
-    }
-
-    suspend fun convertUriToBitmap(uri: Uri): Bitmap? {
-        return withContext(ioDispatcher()) {
-            try {
-                val inputStream = application.contentResolver.openInputStream(uri)
-                if (inputStream != null) {
-                    val bitmap = BitmapFactory.decodeStream(inputStream)
-                    inputStream.close()
-                    bitmap
-                } else {
-                    null
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                null
-            }
         }
     }
 }
